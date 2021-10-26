25755235dfdd81ec223ceba62d011d78bb7c5196
ashraful109 <88344882+ashraful109@users.noreply.github.com>
2021-10-26 11:24:22 +0600

    Add files via upload

---
+++ 1002.py
@@ -0,0 +1,3 @@
+R = float(input())
+a = 3.14159* R*R
+print("A=%.4f"%a)
---
+++ 1006.py
@@ -0,0 +1,5 @@
+A = float(input())
+B = float(input())
+C = float(input())
+Average = (((A*2)+(B*3)+(C*5))/ 10.0)
+print("MEDIA = %.1f"%Average)
