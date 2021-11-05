cba6f49e5da9babf74749b027354dafb15b0bae4
ashraful109 <88344882+ashraful109@users.noreply.github.com>
2021-11-05 09:38:21 +0600

    Add files via upload

---
+++ 1014.py
@@ -0,0 +1,8 @@
+# X,Y = input().split()
+# average_consumption = float(int(X)/float(Y))
+# print("%.3f"%average_consumption,"km/l")
+
+X = float(int(input()))
+Y = float(input())
+average_consumption =X/Y
+print("%.3f"%average_consumption,"km/l")
