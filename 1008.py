4cf9b5bc4a1c463b26b33c2aac51a4147eb2b338
ashraful109 <88344882+ashraful109@users.noreply.github.com>
2021-11-05 09:39:23 +0600

    Add files via upload

---
+++ 1008.py
@@ -0,0 +1,8 @@
+number = int(input()) 
+rate = int(input()) 
+hours = float(input()) 
+salary = rate * hours
+print("NUMBER =",number)
+print("SALARY = U$ %.2f"%salary)
+
+
---
+++ 1009.py
@@ -0,0 +1,6 @@
+name = (input("")) 
+salary = float(input()) 
+sale = float(input())
+comision = sale * 0.15 
+total = comision + salary 
+print("TOTAL = R$ %0.2f"%total)
